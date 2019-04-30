46ec3284d1ae9c02afe44c4325dcd1879f8ca588
ryanden2018 <denlinger.ryan@gmail.com>
2019-04-30 17:55:37 -0500

    done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,12 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    new_params = params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+
+    if new_params[:user_attributes] && (new_params[:user_attributes][:username] == "")
+      new_params.delete("user_attributes")
+    end
+
+    new_params
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
+
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @comments = @post.comments.reload
+    @users = @post.users.reload.uniq
+    @categories = @post.categories.reload
+    @comment = @post.comments.build
+    @all_users = User.all
   end
 
   def index
@@ -9,6 +15,7 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @categories = Category.all
   end
 
   def create
@@ -19,6 +26,12 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+
+    new_params = params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    
+    if new_params[:categories_attributes] && new_params[:categories_attributes]["0"] && (new_params[:categories_attributes]["0"][:name] == "")
+      new_params[:categories_attributes] = {}
+    end
+    new_params
   end
 end
